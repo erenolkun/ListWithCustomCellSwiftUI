5b635c5fe58efd79507cab348eb49c4fe780aed8
erenolkun <erenolkun@gmail.com>
2023-09-03 14:18:51 +0300

    Initial Commit

---
+++ ListWithCustomCellSwift/ListWithCustomCellSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListWithCustomCellSwiftApp.swift
+//  ListWithCustomCellSwift
+//
+//  Created by FERDA OLKUN on 3.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ListWithCustomCellSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
